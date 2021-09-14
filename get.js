cdb49d17345eaea6fce5dd8ac029d482b19afd10
kirankatuwal <kirankatuwal1@gmail.com>
2021-09-14 10:18:39 +0545

    date and time in JS

---
+++ get.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var dt = require('./myfirstmodule');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("The date and times are currently: " + dt.myDateTime());
+    res.write(req.url);
+
+    res.end();
+}).listen(8080);
